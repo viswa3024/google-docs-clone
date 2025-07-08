09f3c976a40178b54c67732f5d7742879cf4a842
Recorichardretardo <hi@hi.com>
2025-07-09 02:26:04 +0530

    ruler added

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -23,6 +23,7 @@ import ImageResize from 'tiptap-extension-resize-image';
 import { useEditorStore } from '@/store/use-editor-store';
 import { FontSizeExtension } from '@/extensions/font-size';
 import { LineHeightExtension } from '@/extensions/line-height';
+import { Ruler } from './ruler';
 
 export default function Editor() {
 
@@ -184,6 +185,7 @@ export default function Editor() {
 
   return (<>
    <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+    <Ruler />
     <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
     </div>
--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -739,7 +739,7 @@ export default function Toolbar() {
         ],
     ];
   return (<>
-   <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
+   <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto print:hidden">
     {sections[0].map((item) => (
         <ToolbarButton key={item.label} {...item}/>
     ))}
