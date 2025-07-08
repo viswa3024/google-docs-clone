03d544aa62c208462b9a775f38dee6615905c6a9
Recorichardretardo <hi@hi.com>
2025-07-08 23:05:50 +0530

    Text alignments and lists tools added

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -16,6 +16,7 @@ import TextStyle from '@tiptap/extension-text-style'
 import { Color } from '@tiptap/extension-color'
 import Highlight from '@tiptap/extension-highlight'
 import Link from '@tiptap/extension-link'
+import TextAlign from '@tiptap/extension-text-align'
 
 import ImageResize from 'tiptap-extension-resize-image';
 
@@ -59,6 +60,9 @@ export default function Editor() {
         immediatelyRender: false,
         extensions: [
             StarterKit,
+            TextAlign.configure({
+                types: ['heading', 'paragraph'],
+            }),
             Color,
             Highlight.configure({ multicolor: true }),
             TextStyle, 
--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -8,6 +8,10 @@ import { type Level } from "@tiptap/extension-heading";
 // AlphaPicker BlockPicker ChromePicker CirclePicker CompactPicker GithubPicker HuePicker MaterialPicker PhotoshopPicker SketchPicker SliderPicker SwatchesPicker TwitterPicker
 
 import { 
+    AlignCenterIcon,
+    AlignJustifyIcon,
+    AlignLeftIcon,
+    AlignRightIcon,
     BoldIcon, 
     ChevronDownIcon, 
     CircleIcon, 
@@ -16,6 +20,7 @@ import {
     ItalicIcon, 
     Link2Icon, 
     List, 
+    ListIcon, 
     ListOrderedIcon, 
     ListTodoIcon, 
     LucideIcon, 
@@ -51,7 +56,104 @@ import {
 
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-import { on } from "events";
+
+const ListButton = () => {
+    const { editor } = useEditorStore();
+
+    const lists = [
+        { 
+            label: "Bullet List",
+            icon: ListIcon,
+            isActive: () => editor?.isActive("bulletList"),
+            onClick: () => editor?.chain().focus().toggleBulletList().run(),
+        },
+        {
+            label: "Ordered List",
+            icon: ListOrderedIcon,
+            isActive: () => editor?.isActive("orderedList"),
+            onClick: () => editor?.chain().focus().toggleOrderedList().run(),
+        },
+        { 
+            label: "Task List",
+            icon: ListTodoIcon,
+            isActive: () => editor?.isActive("taskList"),
+            onClick: () => editor?.chain().focus().toggleTaskList().run(),  
+        }
+        
+    ];
+
+    return (<>
+        <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+            <button className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center ronded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                    title="List Styles"
+            >
+                <ListIcon className="size-4" />
+            </button>     
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="p-1 flex flex-col gap-y-1 border  rounded-none shadow-md">
+           {lists.map(({ label, icon: Icon, onClick, isActive }) => (
+            <button
+                key={label}
+                onClick={onClick}
+                className={cn(
+                    "flex items-center cursor-pointer gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                    isActive() && "bg-neutral-200/80"
+                )}
+            >
+                <Icon className="size-4" />
+                <span className="text-sm">{label}</span>
+            </button>
+           ))}  
+        </DropdownMenuContent>
+        </DropdownMenu>
+    </>)
+};
+
+const AlignButton = () => {
+    const { editor } = useEditorStore();
+
+    const alignments = [
+        { label: "Align Left", value: "left", icon: AlignLeftIcon },
+        { label: "Align Center", value: "center", icon: AlignCenterIcon },
+        { label: "Align Right", value: "right", icon: AlignRightIcon },
+        { label: "Align Justify", value: "justify", icon: AlignJustifyIcon },
+    ];
+
+    return (<>
+        <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+            <button className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center ronded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                    title="Text Alignment"
+            >
+                <AlignLeftIcon className="size-4" />
+            </button>     
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="p-1 flex flex-col gap-y-1 border  rounded-none shadow-md">
+           {alignments.map(({ label, value, icon: Icon }) => (
+            <button
+                key={value}
+                onClick={() => {
+                     editor?.chain().focus().setTextAlign(value).run();
+                    // if (value === "justify") {
+                    //     editor?.chain().focus().setTextAlign("justify").run();
+                    // } else {
+                    //     editor?.chain().focus().setTextAlign(value).run();
+                    // }
+                }}
+                className={cn(
+                    "flex items-center cursor-pointer gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                    editor?.isActive({ textAlign: value }) && "bg-neutral-200/80"
+                )}
+            >
+                <Icon className="size-4" />
+                <span className="text-sm">{label}</span>
+            </button>
+           ))}  
+        </DropdownMenuContent>
+        </DropdownMenu>
+    </>)
+};
 
 const ImageButton = () => {
     const { editor } = useEditorStore();
@@ -465,7 +567,7 @@ export default function Toolbar() {
             },
             {
                 label: "Bullet List",
-                icon: List,
+                icon: ListIcon,
                 isActive: editor?.isActive("bulletList"),
                 onClick: () => editor?.chain().focus().toggleBulletList().run(),
             },
@@ -520,9 +622,9 @@ export default function Toolbar() {
     <Separator orientation="vertical" className="h-6 bg-neutral-300" />
     <LinkButton />
     <ImageButton />
-    {/* TODO: Align */}
+    <AlignButton />
     {/* TODO: Line Height */}
-    {/* TODO: List */}
+    <ListButton />
     {sections[2].map((item) => (
         <ToolbarButton key={item.label} {...item}/>
     ))}
